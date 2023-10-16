3902de8340b66ea7cff84d4c6df55554c7865cd6
Сергей Михальченко <mihalchenko@MacBook-Air-Sergej.local>
2023-10-16 21:04:50 +0300

    done homework on Lesson 7

--- Lesson07/script.js
+++ Lesson07/script.js
@@ -23,19 +23,24 @@ let arrayMax = [3,67,15,21,0,-81];
 console.log (arrayFindMax(arrayMax));
 
 // 3. записать в массив ряд фибоначчи начиная с N члена с длинной массива M
-let fibonacciNum = function (start, length) {
-    let array = [start, start+1];
+let fibonacciNum = function (startNum, length) {
+    
+    function getFibonacci(num) {
+        return num <=1 ? num : getFibonacci(num - 1) + getFibonacci(num - 2);
+    };
+
+    let array = [getFibonacci(startNum), getFibonacci(startNum+1)];
 
     for (i = 2; i < length; i++) {
         let newElement = array[array.length-1] + array[array.length-2];
 
         array.push(newElement);
-    }
+    };
     
     return array;
 };
 
-console.log(fibonacciNum(3,6));
+console.log(fibonacciNum(5,6));
 
 // 4. даны 2 4-х значных числа с неповторяющимися цифрами, надо определить сколько цифр в этих числах совпадают по значению и позиции и сколько только по значению
 let findMatches = function (num1, num2) {
@@ -55,7 +60,36 @@ let findMatches = function (num1, num2) {
         });
     });
 
-    return console.log(`Elements with same position = ${samePos}, elements with same value = ${sameValue}`);
+    return console.log(`Elements with same position and value = ${samePos}, elements with same value(only) = ${sameValue}`);
 };
 
 findMatches(3487,3794);
+
+//5. сортировка массива по возрастанию/убыванию
+let sortArray = function (arr, sortBy) {
+    if (sortBy == "min") {
+        arr.sort((a,b) => a-b);
+    } else if (sortBy == "max") {
+        arr.sort((a,b) => b-a);
+    };
+
+    return arr;
+};
+
+let arrayForSort = [5, 3, 7, 0, 12, -3];
+
+console.log(sortArray(arrayForSort, "min"));
+console.log(sortArray(arrayForSort, "max"));
+
+//6. удалить из массива все повторяющиеся элементы
+let deleteArrElements = function (arr) {
+    let newArr = [];
+    arr.forEach(function(item) {
+        !newArr.includes(item) ? newArr.push(item) : null;
+    });
+    return newArr;
+};
+
+let arrayForDelete = [1,1,8,12,5,1,12,8,0];
+
+console.log(deleteArrElements(arrayForDelete));
