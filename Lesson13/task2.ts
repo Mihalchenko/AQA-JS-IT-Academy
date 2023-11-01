2fb13f33a72253f5689cf447232d22f5530dc20c
Сергей Михальченко <mihalchenko@MacBook-Air-Sergej.local>
2023-11-01 11:56:39 +0300

    done task1

---
+++ Lesson13/task2.ts
@@ -0,0 +1,39 @@
+/**
+2. Создайте интерфейсы для ролей User и Admin, после этого создайте интерйфей Person, который будет соответствовать массиву
+
+type Person = unknown;
+
+const persons: User[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Jane Doe',
+        age: 32,
+        role: 'Administrator'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    },
+    {
+        name: 'Bruce Willis',
+        age: 64,
+        role: 'World saver'
+    }
+];
+ */
+interface User {
+
+};
+
+interface Admin {
+
+};
+
+interface Person {
+
+};
