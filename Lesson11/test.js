991a19b7482796a361844313ae2023d5968d1291
Сергей Михальченко <mihalchenko@MacBook-Air-Sergej.local>
2023-11-09 16:43:30 +0300

    add new features

--- Lesson11/test.js
+++ Lesson11/test.js
@@ -29,5 +29,51 @@ class CarBuilder {
     }
 }
 
+class Taxi {
+    constructor() {
+        this.cars = [];
+    }
+
+    addCar(car, type) {
+        car.type = type;
+        this.cars.push(car);
+    }
+
+    getCars() {
+        return this.cars;
+    }
+}
+
+class Comfort {
+    constructor(car, carMediator) {
+        this.carMediator = carMediator;
+        this.car = car;
+    }
+
+    addCar() {
+        this.carMediator.addCar(this.car, 'comfort');
+    }
+}
+
+class Econom {
+    constructor(car, carMediator) {
+        this.carMediator = carMediator;
+        this.car = car;
+    }
+
+    addCar() {
+        this.carMediator.addCar(this.car, 'econom');
+    }
+}
+
+let taxi = new Taxi();
+
 let VWPolo = new CarBuilder('VW', "Polo").setConsumption(8.2).setPrice(7000).build();
-console.log(VWPolo);
+let VWPassat = new CarBuilder('VW', "Passat").setConsumption(7.6).setPrice(12000).build();
+let BMW3 = new CarBuilder('BMW', "3").setConsumption(5.6).setPrice(8000).build(); 
+
+new Comfort(VWPolo, taxi).addCar();
+new Econom(VWPassat, taxi).addCar();
+new Comfort(BMW3, taxi).addCar();
+
+console.log(taxi.getCars());
