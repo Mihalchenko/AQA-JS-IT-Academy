dcc9117922373aff2acbc1dcdbb5b9c7472608d9
Сергей Михальченко <mihalchenko@MacBook-Air-Sergej.local>
2023-11-09 17:23:19 +0300

    add new features

--- Lesson11/test.js
+++ Lesson11/test.js
@@ -50,7 +50,7 @@ class Comfort {
     constructor(car, carMediator) {
         this.carMediator = carMediator;
         this.car = car;
-        this.car.type = 'comfort';
+        this.car.kmPrice = "3.6";
     }
 
     addCar() {
@@ -62,7 +62,7 @@ class Econom {
     constructor(car, carMediator) {
         this.carMediator = carMediator;
         this.car = car;
-        this.car.type = 'econom';
+        this.car.kmPrice = "2.1";
     }
 
     addCar() {
@@ -74,10 +74,14 @@ let taxi = new Taxi();
 
 let VWPolo = new CarBuilder('VW', "Polo").setConsumption(8.2).setPrice(7000).build();
 let VWPassat = new CarBuilder('VW', "Passat").setConsumption(7.6).setPrice(12000).build();
-let BMW3 = new CarBuilder('BMW', "3").setConsumption(5.6).setPrice(8000).build(); 
+let BMW3 = new CarBuilder('BMW', "3").setConsumption(5.6).setPrice(8000).build();
+let AudiA6 = new CarBuilder('Audi', "A6").setConsumption(11.3).setPrice(15000).build();
+let LadaVesta = new CarBuilder('Lada', "Vesta").setConsumption(9.7).setPrice(6500).build();
 
 new Comfort(VWPolo, taxi).addCar();
 new Econom(VWPassat, taxi).addCar();
 new Comfort(BMW3, taxi).addCar();
+new Comfort(AudiA6, taxi).addCar();
+new Econom(LadaVesta, taxi).addCar();
 
 console.log(taxi.getCars());
