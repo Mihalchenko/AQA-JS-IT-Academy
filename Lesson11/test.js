0a16021436c7dc9658e377e71ddd99c700a0f511
Сергей Михальченко <mihalchenko@MacBook-Air-Sergej.local>
2023-11-10 13:24:02 +0300

    add sortBy consumption function

--- Lesson11/test.js
+++ Lesson11/test.js
@@ -4,71 +4,90 @@ class Car {
         this.model = '';
         this.consumption = 0;
         this.price = 0;
-    }
-}
+    };
+};
 
 class CarBuilder {
     constructor(brand, model) {
         this.car = new Car;
         this.car.brand = brand;
         this.car.model = model;
-    }
+    };
 
     setConsumption(value) {
         this.car.consumption = value;
         return this;
-    }
+    };
 
     setPrice(price) {
         this.car.price = price;
         return this;
-    }
+    };
 
     build() {
         return this.car;
-    }
-}
+    };
+};
 
 class Taxi {
     constructor() {
         this.cars = {};
-    }
+    };
 
     addCar(car, type) {
         if (!this.cars[type]) {
             this.cars[type] = [];
-        }
+        };
         this.cars[type].push(car);
-    }
+    };
 
     getCars() {
         return this.cars;
-    }
-}
+    };
+
+    createCarsArray() {
+        let carsArray = [];
+        for (let key in this.cars) {
+            this.cars[key].map((item) => carsArray.push(item));
+        };
+        return carsArray;
+    };
+
+    sortByConsumption(sortBy = "min") {
+        let carsArray = this.createCarsArray();
+        if (sortBy == "max") {
+            carsArray.sort((a,b) => b.consumption - a.consumption);
+        } else if (sortBy == "min") {
+            carsArray.sort((a,b) => a.consumption - b.consumption);
+        };
+
+        return carsArray;
+    };
+};
 
 class Comfort {
     constructor(car, carMediator) {
         this.carMediator = carMediator;
         this.car = car;
         this.car.kmPrice = "3.6";
-    }
+    };
 
     addCar() {
         this.carMediator.addCar(this.car, 'comfort');
-    }
-}
+    };
+};
 
 class Econom {
     constructor(car, carMediator) {
         this.carMediator = carMediator;
         this.car = car;
         this.car.kmPrice = "2.1";
-    }
+    };
 
     addCar() {
         this.carMediator.addCar(this.car, 'econom');
-    }
-}
+    };
+};
 
 let taxi = new Taxi();
 
@@ -85,3 +104,4 @@ new Comfort(AudiA6, taxi).addCar();
 new Econom(LadaVesta, taxi).addCar();
 
 console.log(taxi.getCars());
+console.log(taxi.sortByConsumption('max'));
