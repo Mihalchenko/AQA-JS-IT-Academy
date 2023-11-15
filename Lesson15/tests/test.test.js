8af6ab83b7c462746296e83c05bb3b1d5c4fbe0e
Сергей Михальченко <mihalchenko@MacBook-Air-Sergej.local>
2023-11-15 16:48:28 +0300

    try to test add() function

--- Lesson15/tests/test.test.js
+++ Lesson15/tests/test.test.js
@@ -1,2 +1,20 @@
 const Calculator = require('../calculator');
+const calcMethods = new Calculator;
 
+describe(`Calculator tests`, function () {
+    
+    describe(`Add function tests`, function () {
+        let testData = [[1,2,3], [5,6,11]];
+        
+        test.each(testData)(`%i + %i should be equal %i`, async (firstNum, secondNum, sum) => {
+            const result = calcMethods.add(firstNum, secondNum);
+            await expect(result).toEqual(sum);
+        });
+
+        test.each(testData)(`%i + %i should be type "number"`, async (firstNum, secondNum, sum) => {
+            const result = calcMethods.add(firstNum, secondNum);
+            await expect(typeof result).toBe("number");
+        });
+    });
+
+});
