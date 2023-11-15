50df96a7be7d01e6922819707e275ba2a52d9e28
Сергей Михальченко <mihalchenko@MacBook-Air-Sergej.local>
2023-11-15 20:15:33 +0300

    done tests for 1-3 functions

--- Lesson15/tests/test.test.js
+++ Lesson15/tests/test.test.js
@@ -4,17 +4,55 @@ const calcMethods = new Calculator;
 describe(`Calculator tests`, function () {
     
     describe(`Add function tests`, function () {
-        let testData = [[1,2,3], [5,6,11]];
+        const testData = [
+            {numbers: [1,2,9], total: 12},
+            {numbers: [6,7,3,1], total: 17}
+        ];
         
-        test.each(testData)(`%i + %i should be equal %i`, async (firstNum, secondNum, sum) => {
-            const result = calcMethods.add(firstNum, secondNum);
-            await expect(result).toEqual(sum);
+        test.each(testData)(`Should return $total after summing $numbers`, async ({numbers, total}) => {
+            console.log(Array.isArray(numbers));
+            const result = calcMethods.add(numbers);
+            await expect(result).toEqual(total);
         });
 
-        test.each(testData)(`%i + %i should be type "number"`, async (firstNum, secondNum, sum) => {
-            const result = calcMethods.add(firstNum, secondNum);
+        test.each(testData)(`Should return type "number" after summing nums $numbers`, async ({numbers}) => {
+            const result = calcMethods.add(numbers);
             await expect(typeof result).toBe("number");
         });
     });
 
+    describe(`Multiply function tests`, function () {
+        const testData = [
+            {numbers: [1,2,9], total: 18},
+            {numbers: [6,7,3,1], total: 126}
+        ];
+        
+        test.each(testData)(`Should return $total after multiplying $numbers`, async ({numbers, total}) => {
+            console.log(Array.isArray(numbers));
+            const result = calcMethods.multiply(numbers);
+            await expect(result).toEqual(total);
+        });
+
+        test.each(testData)(`Should return type "number" after multiplying $numbers`, async ({numbers}) => {
+            const result = calcMethods.multiply(numbers);
+            await expect(typeof result).toBe("number");
+        });
+    });
+
+    describe(`Subtraction functions tests`, function () {
+        const testData = [
+            [7,4,3],
+            [10,3,7]
+        ];
+
+        test.each(testData)(`Substrucion %i from %i should return %i`, async (a,b,sum) => {
+            const result = calcMethods.subtraction(a,b);
+            await expect(result).toEqual(sum);
+        });
+
+        test.each(testData)(`Should return type "number" after substruction %i and %i`, async (a,b) => {
+            const result = calcMethods.subtraction(a,b);
+            await expect(typeof result).toBe("number");
+        });
+    });
 });
