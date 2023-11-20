7e660a15f4178c222b86d21844aac02a8187a514
Сергей Михальченко <mihalchenko@MacBook-Air-Sergej.local>
2023-11-20 19:17:09 +0300

    Done all tests

--- Lesson16/tests/test.test.js
+++ Lesson16/tests/test.test.js
@@ -4,12 +4,13 @@ const validate = new Validator();
 const apiSchema = require('../data/fakeAPIschema.json');
 
 describe(`fakeAPI tests`, function () {
+    const apiURL = 'https://fakerestapi.azurewebsites.net/api/v1/Activities';
 
     describe(`GET method tests`, function () {
         let response;
 
         beforeAll(async() => {
-            response = await axios.get('https://fakerestapi.azurewebsites.net/api/v1/Activities');
+            response = await axios.get(apiURL);
         });
 
         test(`Should return status code 200`, async () => { 
@@ -26,7 +27,7 @@ describe(`fakeAPI tests`, function () {
         let response;
 
         beforeAll(async() => {
-            response = await axios.post('https://fakerestapi.azurewebsites.net/api/v1/Activities', {
+            response = await axios.post(apiURL, {
                 "id": 0,
                 "title": "string",
                 "dueDate": "2023-11-20T12:12:21.962Z",
@@ -48,9 +49,8 @@ describe(`fakeAPI tests`, function () {
         let response;
 
         beforeAll(async() => {
-            response = await axios.get('https://fakerestapi.azurewebsites.net/api/v1/Activities/', {
-                params: {id: 2}
-            });
+            const id = 2;
+            response = await axios.get(`${apiURL}/${id}`);
         });
 
         test(`Should return status code 200`, async () => { 
@@ -58,10 +58,48 @@ describe(`fakeAPI tests`, function () {
         });
     
         test(`Should be valid response body`, async () => {
-            console.log(response);
             const result = validate.validate(response.data, apiSchema);
             expect(result.valid).toBe(true);
         });
     });
     
+    describe(`PUT by ID method tests`, function () {
+        let response;
+
+        beforeAll(async() => {
+            const id = 2;
+            response = await axios.put(`${apiURL}/${id}`, {
+                "id": 0,
+                "title": "string",
+                "dueDate": "2023-11-20T16:07:57.473Z",
+                "completed": true
+              });
+        });
+
+        test(`Should return status code 200`, async () => { 
+            expect(response.status).toBe(200);
+        });
+    
+        test(`Should be valid response body`, async () => {
+            const result = validate.validate(response.data, apiSchema);
+            expect(result.valid).toBe(true);
+        });
+    });
+
+    describe(`DELETE by ID method tests`, function () {
+        let response;
+
+        beforeAll(async() => {
+            const id = 2;
+            response = await axios.delete(`${apiURL}/${id}`);
+        });
+
+        test(`Should return status code 200`, async () => { 
+            expect(response.status).toBe(200);
+        });
+    
+        test(`Should be empty response body`, async () => {
+            expect(response.data).toBe("");
+        });
+    });
 });
