f58a0fa43353dfc9d12e2d2edded2f1dc0d01331
Сергей Михальченко <mihalchenko@MacBook-Air-Sergej.local>
2023-11-27 17:02:45 +0300

    Add some tests

--- Lesson18/test/test.test.js
+++ Lesson18/test/test.test.js
@@ -20,7 +20,21 @@ describe('Chromdriver website tests', () => {
         expect(await mainTitle.getText()).to.equal('ChromeDriver');
     });
 
-    it(`Title on page "Extensions" should be "Chrome Extensions"`, async() => {
-        const extensionButton = await driver.findElement(By.css(`nav#WDxLfe a[data-url="/extensions"]`));
+    it(`Title on page "Extensions" should be "Chrome Extensions"`, async() => {        
+        let browserSize = await driver.manage().window().getSize();
+        if (browserSize.width < 1234) {
+            const moreMenuButton = await driver.findElement(By.css('nav#WDxLfe li[more-menu-item]'));
+            await moreMenuButton.click();
+            const extensionButton = await driver.findElement(By.css('nav#WDxLfe li[more-menu-item] a[data-url="/extensions"]'));
+            await extensionButton.click();
+        } else {
+            const extensionButton = await driver.findElement(By.css(`nav#WDxLfe li[data-nav-level="1"] > div > div a[data-url="/extensions"]`));
+            await extensionButton.click();
+        }
+
+        const mainTitle = await driver.findElement(By.css(`div[role="main"] h1 span`));
+        await driver.executeScript("arguments[0].style.backgroundColor='red'", mainTitle);
+
+        expect(await mainTitle.getText()).to.equal('Chrome Extensions');
     });
 });
