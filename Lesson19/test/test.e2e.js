9d24849c1f0c2dade6cddb3e2d6cbbdbb919f519
Сергей Михальченко <mihalchenko@MacBook-Air-Sergej.local>
2023-12-04 20:57:09 +0300

    add ESLint

--- Lesson19/test/test.e2e.js
+++ Lesson19/test/test.e2e.js
@@ -1,40 +1,42 @@
-import {expect} from 'chai';
+/* eslint-disable no-undef */
+import { expect } from 'chai';
 
 describe('wdio website testing', () => {
-    it('Title on page API should be "Introduction"', async () => {
-        await browser.url('https://webdriver.io/');
-        const apiHeaderButton = await $('.navbar__items a[href="/docs/api"]');
-        await apiHeaderButton.click();
-        expect(await $('main h1').getText()).to.equal('Introduction');
-    });
+  it('Title on page API should be "Introduction"', async () => {
+    await browser.url('https://webdriver.io/');
+    const apiHeaderButton = await $('.navbar__items a[href="/docs/api"]');
+    await apiHeaderButton.click();
+    expect(await $('main h1').getText()).to.equal('Introduction');
+  });
 
-    it('Search by "allure" should return "Allure reporter" page', async () => {
-        const searchButton = await $('.DocSearch.DocSearch-Button');
-        await searchButton.click();
-        const searchInput = await $('input.DocSearch-Input');
-        await searchInput.setValue('allure');
-        const searchResultFirstLink = await $('li#docsearch-item-0 a');
-        await searchResultFirstLink.click();
-        expect(await $('main h1').getText()).to.equal('Allure Reporter');
-    });
+  it('Search by "allure" should return "Allure reporter" page', async () => {
+    const searchButton = await $('.DocSearch.DocSearch-Button');
+    await searchButton.click();
+    const searchInput = await $('input.DocSearch-Input');
+    await searchInput.setValue('allure');
+    const searchResultFirstLink = await $('li#docsearch-item-0 a');
+    await searchResultFirstLink.click();
+    expect(await $('main h1').getText()).to.equal('Allure Reporter');
+  });
 
-    it('Switch to dark theme should switch html data-theme to "dark"', async () => {
-        const switchThemeButton = await $('.colorModeToggle_DEke');
-        await switchThemeButton.click();
-        expect(await $('html[data-theme]').getAttribute('data-theme')).to.equal('dark');
-    });
+  it('Switch to dark theme should switch html data-theme to "dark"', async () => {
+    const switchThemeButton = await $('.colorModeToggle_DEke');
+    await switchThemeButton.click();
+    expect(await $('html[data-theme]').getAttribute('data-theme')).to.equal('dark');
+  });
 
-    it('Changing language to "Deutch" should return "Deutch"', async () => {
-        const langDropdownMenu = await $('.navbar__item.dropdown');
-        await langDropdownMenu.click();
-        const deLangButton = await $('.dropdown__menu a[lang="de"]');
-        await deLangButton.click();
-        expect(await $('.navbar__item.dropdown a.navbar__link').getText()).to.equal('Deutsch');
-    });
+  it('Changing language to "Deutch" should return "Deutch"', async () => {
+    const langDropdownMenu = await $('.navbar__item.dropdown');
+    await langDropdownMenu.click();
+    const deLangButton = await $('.dropdown__menu a[lang="de"]');
+    await deLangButton.click();
+    expect(await $('.navbar__item.dropdown a.navbar__link').getText()).to.equal('Deutsch');
+  });
 
-    it('Title on page API with Deutch lang should be "Einleitung"', async () => {
-        const apiHeaderButton = await $('.navbar__items a[href="/de/docs/api"]');
-        await apiHeaderButton.click();
-        expect(await $('main h1').getText()).to.equal('Einleitung');
-    });
+  it('Title on page API with Deutch lang should be "Einleitung"', async () => {
+    const apiHeaderButton = await $('.navbar__items a[href="/de/docs/api"]');
+    await apiHeaderButton.click();
+    // ТУТ ОЖИДАЛКУ
+    expect(await $('main h1').getText()).to.equal('Einleitung');
+  });
 });
