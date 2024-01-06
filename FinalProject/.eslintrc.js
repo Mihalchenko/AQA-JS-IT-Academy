00ab67c8fbcafd1a2e084beffde9fc459447a038
Сергей Михальченко <mihalchenko@MacBook-Air-Sergej.local>
2024-01-07 00:16:01 +0300

    add some eslint rules

--- FinalProject/.eslintrc.js
+++ FinalProject/.eslintrc.js
@@ -30,5 +30,83 @@ module.exports = {
         "it": true
     },
     "rules": {
+        'no-restricted-syntax': 0,
+        'no-await-in-loop': 'off',
+        'import/extensions': 'off',
+        'import/no-extraneous-dependencies': 'off',
+        'consistent-return': 'off',
+        'lines-between-class-members': 0,
+        'import/prefer-default-export': 0,
+        'class-methods-use-this': 0,
+        'import/no-cycle': 0,
+        'camelcase': 'off',
+        'no-useless-constructor': 0,
+        'guard-for-in': 'off',
+        'max-len': [
+            2,
+            {
+                'code': 300
+            }
+        ],
+        'no-multi-spaces': [
+            'error',
+            {
+                'ignoreEOLComments': true
+            }
+        ],
+        'no-console': [
+            'error',
+            {
+                'allow': [
+                    'warn',
+                    'error',
+                    'info'
+                ]
+            }
+        ],
+        'no-shadow': 'off',
+        'no-empty-function': ['error', { 'allow': ['constructors'] }],
+        'prefer-destructuring': ['error', {'object': false, 'array': false}],
+        'no-undefined': 2,
+        'no-unused-vars': 1,
+        'no-plusplus': 'off',
+        'newline-per-chained-call': [
+            'error',
+            {
+                'ignoreChainWithDepth': 3
+            }
+        ],
+        'linebreak-style': [
+            'off'
+        ],
+        'arrow-parens': 'off',
+        'func-names': [
+            'off'
+        ],
+        'no-param-reassign': [
+            'off'
+        ],
+        'prefer-arrow-callback': [
+            'off'
+        ],
+        'dot-notation': [
+            'error',
+            {
+                'allowPattern': '[a-zA-Z]'
+            }
+        ],
+        'no-underscore-dangle': [
+            'error',
+            {
+                'allow': [
+                    '___initialData___',
+                    '_request',
+                    '_method',
+                    '_baseSelector',
+                    '_bodenPoint',
+                    '_baseSelectorCity'
+                ]
+            }
+        ]
     }
 }
