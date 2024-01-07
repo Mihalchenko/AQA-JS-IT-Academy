cb942f31fafd6ab1114f055b3097c9eb4b327d7a
Сергей Михальченко <mihalchenko@MacBook-Air-Sergej.local>
2024-01-07 12:56:27 +0300

    increase defaultCommandTimeout from 8000 to 10000

--- FinalProject/cypress.config.js
+++ FinalProject/cypress.config.js
@@ -4,7 +4,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1280,
-  defaultCommandTimeout: 8000,
+  defaultCommandTimeout: 10000,
   e2e: {
     testIsolation: false,
     setupNodeEvents(on, config) {
