b1ad62cd7d765a73db087d38175d463b77a1871c
Сергей Михальченко <mihalchenko@MacBook-Air-Sergej.local>
2024-01-07 14:51:21 +0300

    add compare test

--- FinalProject/cypress/e2e/compareItems.spec.js
+++ FinalProject/cypress/e2e/compareItems.spec.js
@@ -7,15 +7,21 @@ describe('Onliner compare catalog items tests', () => {
   before(() => {
     cy.clearCookie("compare");
   });
+  let numberForCompare = 3;
 
   it('Add items to compare should show compare button', () => {
     homePage.navigate('https://catalog.onliner.by/notebook');
-    catalogItemsPage.addItemsToCompare(3);
+    catalogItemsPage.addItemsToCompare(numberForCompare);
     catalogItemsPage.compareButton.should('exist');
   });
 
   it('Click on compare button should open compare page with "Cравнение товаров" title', () => {
     catalogItemsPage.compareButton.click();
     compareItemsPage.pageMainTitle.should('contain', 'Сравнение товаров');
+  });
+
+  it(`Deleting one of items from compare should return ${numberForCompare-1} items`, () => {
+    compareItemsPage.deleteItemsFromCompareButtons.first().click();
+    compareItemsPage.itemsInCompare.should('have.lengthOf', numberForCompare-1);
   });
 });
--- FinalProject/page-objects/compareItemsPage.js
+++ FinalProject/page-objects/compareItemsPage.js
@@ -8,6 +8,14 @@ class CompareItemsPage extends Base {
   get pageMainTitle() {
     return cy.get('h1.b-offers-title');
   }
+
+  get itemsInCompare() {
+    return cy.get('tr.product-table__row_top div.product-summary');
+  }
+
+  get deleteItemsFromCompareButtons() {
+    return cy.get('tr.product-table__row_top a.product-icon_trash');
+  }
 }
 
 export default new CompareItemsPage();
