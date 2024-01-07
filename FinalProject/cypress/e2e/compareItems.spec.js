aa253af5641ecc482ea288de61f74910c6f0952b
Сергей Михальченко <mihalchenko@MacBook-Air-Sergej.local>
2024-01-07 15:32:28 +0300

    add more compare tests

--- FinalProject/cypress/e2e/compareItems.spec.js
+++ FinalProject/cypress/e2e/compareItems.spec.js
@@ -7,7 +7,7 @@ describe('Onliner compare catalog items tests', () => {
   before(() => {
     cy.clearCookie("compare");
   });
-  let numberForCompare = 3;
+  let numberForCompare = 4;
 
   it('Add items to compare should show compare button', () => {
     homePage.navigate('https://catalog.onliner.by/notebook');
@@ -21,7 +21,12 @@ describe('Onliner compare catalog items tests', () => {
   });
 
   it(`Deleting one of items from compare should return ${numberForCompare-1} items`, () => {
-    compareItemsPage.deleteItemsFromCompareButtons.first().click();
+    compareItemsPage.deleteOneItemFromCompare();
     compareItemsPage.itemsInCompare.should('have.lengthOf', numberForCompare-1);
+  });
+
+  it(`Deleting all exept one of items from compare should exclude delete item button`, () => {
+    compareItemsPage.leaveOneItemInCompare();
+    compareItemsPage.deleteItemsFromCompareButtons.should('not.exist');
   });
 });
--- FinalProject/page-objects/compareItemsPage.js
+++ FinalProject/page-objects/compareItemsPage.js
@@ -16,6 +16,20 @@ class CompareItemsPage extends Base {
   get deleteItemsFromCompareButtons() {
     return cy.get('tr.product-table__row_top a.product-icon_trash');
   }
+
+  deleteOneItemFromCompare() {
+    this.deleteItemsFromCompareButtons.first().click();
+  }
+
+  leaveOneItemInCompare() {
+    this.itemsInCompare.then((arr) => {
+        let leng = arr.length;
+        while (leng !== 1) {
+            this.deleteOneItemFromCompare();
+            leng-=1;
+        }
+    });
+  }
 }
 
 export default new CompareItemsPage();
